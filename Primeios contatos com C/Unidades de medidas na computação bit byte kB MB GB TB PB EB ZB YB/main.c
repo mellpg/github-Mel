7325a05e819edd255950b8161cfec617d132759e
Mel pereira <melpg16@gmail.com>
2026-02-07 16:45:36 -0300

    Delete Primeios contatos com C directory

--- Primeios contatos com C/Unidades de medidas na computação bit byte kB MB GB TB PB EB ZB YB/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-  /*//  Como descobrir o tamanho em memória de cada tipo de dado? operador sizeof x ou sizeof(int)
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
-*/
--- Primeios contatos com C/aula 3/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("ola!como se chama?\n\n");
-    printf("como anda seu dia?\n\n");
-    return 0;
-}
